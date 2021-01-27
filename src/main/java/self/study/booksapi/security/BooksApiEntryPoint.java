88d56aef2c13a8d8c51c13aefbb699b9e43c187b
Nader <na511dr@hotmail.com>
2021-01-27 04:26:59 +0300

    Configure security properties of the api

---
+++ src/main/java/self/study/booksapi/security/BooksApiEntryPoint.java
@@ -0,0 +1,10 @@
+package self.study.booksapi.security;
+
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BooksApiEntryPoint extends BasicAuthenticationEntryPoint {
+
+
+}
--- src/main/java/self/study/booksapi/security/SecurityConfig.java
+++ src/main/java/self/study/booksapi/security/SecurityConfig.java
@@ -1,9 +1,30 @@
 package self.study.booksapi.security;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private BCryptPasswordEncoder passwordEncoder;
-    
+    private BooksApiEntryPoint entryPoint;
+    private UserDetailsServiceImpl userDetailsService;
+
+
+    public SecurityConfig(BCryptPasswordEncoder passwordEncoder, BooksApiEntryPoint entryPoint, UserDetailsServiceImpl userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.entryPoint = entryPoint;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests().anyRequest()
+                .authenticated().and().httpBasic().authenticationEntryPoint(entryPoint);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
 }
