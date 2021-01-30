2990f88eb25c5fb124a72feedec2d2e08beec0d6
Nader <na511dr@hotmail.com>
2021-01-30 23:13:08 +0300

    Added security entry points

--- src/main/java/self/study/booksapi/security/BooksApiEntryPoint.java
+++ src/main/java/self/study/booksapi/security/BooksApiEntryPoint.java
@@ -1,10 +1,30 @@
 package self.study.booksapi.security;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 @Component
 public class BooksApiEntryPoint extends BasicAuthenticationEntryPoint {
 
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws java.io.IOException {
+        response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        PrintWriter writer = response.getWriter();
+        writer.println("please use correct credentials to access the API");
+    }
 
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("BasicRealm");
+        super.afterPropertiesSet();
+    }
 }
